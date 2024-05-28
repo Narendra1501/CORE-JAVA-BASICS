f695ab1f969a3b57921e818330ebb5467dc8df32
HP <HP@NAREN-HP-VICTUS>
2024-05-28 18:53:18 +0530

    This project is reference for a java basics

---
+++ javacodebasics/src/javabasicsday6/EmployeeDemo.java
@@ -0,0 +1,15 @@
+package javabasicsday6;
+
+public class EmployeeDemo {
+	
+public static void main(String[] args) {
+		
+		Employee e1=new Employee();
+		
+		System.out.println(e1);
+		
+	}
+
+
+	}
+
