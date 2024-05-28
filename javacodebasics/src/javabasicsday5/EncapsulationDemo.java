6b7318f606db19ccfd02ba289d24d504565a71ea
HP <HP@NAREN-HP-VICTUS>
2024-05-28 17:57:51 +0530

    This project is reference for a JavaBasics

---
+++ javacodebasics/src/javabasicsday5/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package javabasicsday5;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 OopsConcept obj = new OopsConcept();
+		   obj.setSerialNum(15);
+		   obj.setName("narendra");
+		   obj.setAge(17);
+		  System.out.println(obj);
+			}
+
+	}
+
+
